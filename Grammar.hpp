849cc4d06eeb5716838c5c658b9c8464f5ecc267
Wave <falsed612@gmail.com>
2018-05-09 20:17:57 +0800

    Create Grammar.hpp

---
+++ Grammar.hpp
@@ -0,0 +1,22 @@
+//
+//  Grammar.hpp
+//  Compiler
+//
+//  Created by Wave on 2018/4/4.
+//  Copyright © 2018年 Wave. All rights reserved.
+//
+
+#ifndef Grammar_hpp
+#define Grammar_hpp
+
+#include <string>
+
+using namespace std;
+
+class tokenOut;
+
+vector<vector<string>> readProd(string path);
+vector<vector<string>> readTable(string path);
+vector<string> Grammar(vector<tokenOut>* tokens, vector<vector<string>> table, vector<vector<string>> prod);
+
+#endif /* Grammar_hpp */
